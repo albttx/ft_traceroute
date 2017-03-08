bd4a333718077865a45c56be72c4fdc940f0a009
Albert Le Batteux <albert.lebatteux@icloud.com>
2017-03-08 15:18:12 +0100

    prpesque plus de jus

--- working.c
+++ working.c
@@ -3,12 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/ip.h> 
-#include <netinet/udp.h> 
 #include <netinet/ip_icmp.h> 
 
-#define PCKT_LEN 512 
-#define PORT (32768 + 666)
-/*#define PCKT_LEN 8192 */
+#define PCKT_LEN 8192 
 #define GOOGLE "216.58.213.163"
 
 static char	*pr_icmp_err_code(struct icmp *icmp)
@@ -40,51 +37,36 @@ static char	*pr_icmp_err_code(struct icmp *icmp)
 }
 
 int		main(int argc, char *argv[]) {
-	int					send_sock;
-	int					recv_sock;
+	int					send;
+	int					recv;
 	int					resp;
 	int					ttl = 1;
 	char				buffer[PCKT_LEN];
 	struct sockaddr_in	dst;
-	struct ip			*ip;
-	struct udphdr		*udp_h;
-	/*struct icmphdr		*icmp_h;*/
 
 	bzero(&buffer, sizeof(buffer));
 	bzero(&dst, sizeof(dst));
 
-	if ((send_sock = socket(PF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
+	if ((send = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
 		perror("Could not process socket() [send].");
 		return EXIT_FAILURE;
 	}
 
-	if ((recv_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0) {
+	if ((recv = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0) {
 		perror("Could not process socket() [recv].");
 		return EXIT_FAILURE;
 	}
 
 	dst.sin_family      = AF_INET;
 	dst.sin_addr.s_addr = inet_addr(GOOGLE);
-	dst.sin_port        = htons(PORT);
-	ip = (struct ip *)buffer;
-	udp_h = (struct udphdr *)((void *)buffer + sizeof(*ip));
-	/*icmp_h = (struct icmphdr *)((void *)buffer + sizeof(*ip));*/
-	
-	ip->ip_off = 0;
-	ip->ip_hl = sizeof(*ip) >> 2;
-	ip->ip_p = IPPROTO_UDP;
-	ip->ip_len = sizeof(struct ip) + sizeof(struct udphdr);
-	ip->ip_ttl = 1;
-	ip->ip_v = IPVERSION;
-	ip->ip_id = 42;
+	dst.sin_port        = htons(32768 + 666);
 
-	udp_h->uh_sport = htons(42);
-	udp_h->uh_dport = htons(PORT + 0);
-	udp_h->uh_ulen = htons((unsigned short)(sizeof(struct udphdr)));
-	/*udp_h->uh_ulen = htons((unsigned short)(datalen - sizeof(struct ip)));*/
-	udp_h->uh_sum = 0;
+	if (setsockopt(send, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
+		perror("Could not process setsockopt().");
+		return EXIT_FAILURE;
+	}
 
-	if (sendto(send_sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&dst, sizeof(dst)) < 0) {
+	if (sendto(send, buffer, sizeof(buffer), 0, (struct sockaddr *)&dst, sizeof(dst)) < 0) {
 		perror("Could not process sendto().");
 		return EXIT_FAILURE;
 	}
@@ -93,28 +75,37 @@ int		main(int argc, char *argv[]) {
 	struct sockaddr_in	from;
 	socklen_t			size;
 	int					cc;
+	struct ip			*ip;
 	struct icmp			*icmp;
 
-	ip = NULL;
-	icmp = NULL;
 	bzero(buff, sizeof(buff));
 	bzero(&from, sizeof(from));
 	bzero(&size, sizeof(size));
 
 	size = sizeof(from);
-	printf("wait to receive...\n");
-	if ((cc = recvfrom(recv_sock, buff, PCKT_LEN, 0, (struct sockaddr *)&from, &size)) < 0) {
+	if ((cc = recvfrom(recv, buff, PCKT_LEN, 0, (struct sockaddr *)&from, &size)) < 0) {
 		perror("recvfrom:");
 		return EXIT_FAILURE;
 	}
-	printf("cc = %d\n", cc);
+	printf("cc = %d\n\n", cc);
 
 	ip = (struct ip *)buff;
 	icmp = (void *)ip + sizeof(*ip);
 
-	printf("icmp err %s \n", pr_icmp_err_code(icmp));
-	printf("ip->ip_ttl = %d \n", ip->ip_ttl);
-	printf("ip->ip_v   = %d \n", ip->ip_v);
+	printf("sizeof ip       = %ld\n", sizeof(struct ip));
+	printf("sizeof iphdr    = %ld\n", sizeof(struct iphdr));
+	printf("sizeof icmp     = %ld\n", sizeof(struct icmp));
+	printf("sizeof icmphdr  = %ld\n", sizeof(struct icmphdr));
+	printf("\n");
+	printf("icmp err    = %s \n", pr_icmp_err_code(icmp));
+	printf("icmp->icmp_type  = %d \n", icmp->icmp_type);
+	printf("icmp->icmp_code  = %d \n", icmp->icmp_code);
+
+	printf("ip->ip_ttl  = %d \n", ip->ip_ttl);
+	printf("ip->ip_v    = %d \n", ip->ip_v);
+	printf("ip->ip_hl   = %d \n", ip->ip_hl);
+	printf("ip->ip_tos  = %d \n", ip->ip_tos);
+	printf("ip->ip_p    = %d \n", ip->ip_p);
 
 
 	/*printf("src:[%s] ", inet_ntoa(*(struct in_addr *)&ip->ip_src.s_addr));*/
